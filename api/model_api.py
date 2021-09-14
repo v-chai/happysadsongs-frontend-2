2500d793036e74db06fec47784d3c7854151a375
Eduardo Fakiani <efakiani@gmail.com>
2021-09-14 14:22:20 -0300

    test

--- api/model_api.py
+++ api/model_api.py
@@ -7,6 +7,8 @@ def PredictTop(list):
         if len(tmp) < 20 or song['Language'] is 'en':
             continue
         r = requests.get(f'{model_base_url}{tmp[:1500]}')
-        print(r)
-        print(r.json())
+        if r.status_code == 200:
+            print(r.json())
+        else:
+            print(song)
     return None
