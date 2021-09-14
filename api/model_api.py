e4ee9114ebd09166aa8ea65c6ab54be3e784dfe3
Eduardo Fakiani <efakiani@gmail.com>
2021-09-14 14:16:02 -0300

    let

--- api/model_api.py
+++ api/model_api.py
@@ -6,7 +6,7 @@ def PredictTop(list):
         tmp = song['Lyrics']
         if len(tmp) < 20:
             continue
-        r = requests.get(f'{model_base_url}{tmp}')
+        r = requests.get(f'{model_base_url}{tmp[:1500]}')
         print(r)
         print(r.json())
     return None
