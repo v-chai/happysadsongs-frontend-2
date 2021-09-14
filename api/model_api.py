e61cf33a455b7c5c330300c6bbb086d43128fd11
Eduardo Fakiani <efakiani@gmail.com>
2021-09-14 14:50:07 -0300

    let

--- api/model_api.py
+++ api/model_api.py
@@ -5,7 +5,7 @@ def PredictTop(list):
     print("_________________ PREDICT STARTED _________________")
     for song in list.values():
         tmp = song['Lyrics']
-        if len(tmp) < 20 or song['Language'] is not 'en':
+        if len(tmp) < 20 or song['Language'] != "en":
             continue
         r = requests.get(f'{model_base_url}{tmp[:1500]}')
         print(r)
