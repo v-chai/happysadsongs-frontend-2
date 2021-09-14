3c467993c42b799ced964722539dc0e2e02ebc3b
Eduardo Fakiani <efakiani@gmail.com>
2021-09-14 14:32:21 -0300

    play

--- api/model_api.py
+++ api/model_api.py
@@ -4,7 +4,7 @@ from api.keys import model_base_url
 def PredictTop(list):
     for song in list.values():
         tmp = song['Lyrics']
-        if len(tmp) < 20 or song['Language'] is 'en':
+        if len(tmp) < 20 or song['Language'] is not 'en':
             continue
         r = requests.get(f'{model_base_url}{tmp[:1500]}')
         if r.status_code == 200:
