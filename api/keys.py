70f532c1819d3260c1d618bf6af21468719292c4
V Prentice <vcoprentice@gmail.com>
2021-09-14 14:26:53 -0700

    fix url

--- api/keys.py
+++ api/keys.py
@@ -5,7 +5,7 @@ genius_id = 'Q64jLUi4wgqTlK7Hu-AA8fClPdmaUFHASM5KUlfe-xXsYsmzwNmU73hZSsdmlzV0'
 genius_sect = 'D5I2w_36VWwZ9CEhn2PErdUdhHWPY5loN90q6kMRUlqZqdH26Mr7Nom2oLevvl67jy3ol1wAvb_CEjQTXJoEug'
 genius_auth_token = 'sueuQT1e0jlB8xwdZs65lQxlIaEuvwdRPtVbdtkfTg-TmrNmREkAKe2YH2NDcQRW'
 
-base_url = 'https://happysadsongs-frontend.herokuapp.com'
-return_url = 'https://happysadsongs-frontend.herokuapp.com/login/authorized'
+base_url = 'https://happysadsongs.herokuapp.com'
+return_url = 'https://happysadsongs.herokuapp.com/login/authorized'
 
 model_base_url = 'https://happysad-lyzudn6c6a-uc.a.run.app/predict'
