f3c99f16dcf88f77092299eccddc1cb3b9c8638e
V Prentice <vcoprentice@gmail.com>
2021-09-14 20:42:17 -0700

    tweaking text and output if no lyrics are tested

--- api/model_api.py
+++ api/model_api.py
@@ -6,6 +6,7 @@ def PredictTop(list):
     checked_songs = []
     preds = []
     valence = []
+    backup_valence = []
     for song in list.values():
         tmp = song['Lyrics']
         if len(tmp) < 20 or song['Language'] != "en":
@@ -15,12 +16,16 @@ def PredictTop(list):
             preds.append(r.json()['prediction'])
             checked_songs.append(f"{song['Artist Names'][0]} - {song['Song Name']}")
             valence.append(float(song["Valence"]))
+            backup_valence.append(float(song["Valence"]))
         else:
-            continue
-    try:
-        overall_pred = int(mode(preds))
-    except:
-        overall_pred = 1
-    avg_valence = mean(valence)
-
+            backup_valence.append(float(song["Valence"]))
+    if len(checked_songs) > 0:
+        try:
+            overall_pred = int(mode(preds))
+        except:
+            overall_pred = 1
+        avg_valence = mean(valence)
+    else:
+        avg_valence = mean(backup_valence)
+        overall_pred = "Could not analyze any lyrics."
     return checked_songs, overall_pred, avg_valence
